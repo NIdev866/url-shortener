1c35baa5f994cebda0a247f8027debcc51e846d5
NIdev866 <nidev866@gmail.com>
2017-08-13 02:37:57 +0100

    fixed

--- configuration.js
+++ configuration.js
@@ -1,8 +1,8 @@
 const obj = {}
 
 obj.d = {}
-obj.host = 'http://localhost:8080/'
-obj.d.host = 'localhost'
+obj.host = 'https://nidev866-url-shortener.herokuapp.com/'
+obj.d.host = 'https://nidev866-url-shortener.herokuapp.com/'
 obj.d.name = 'url_shortener'
 
 module.exports = obj
